bf4763575800d4bc2bbd4a606d594796cf9efba5
dhha <doanhuuha@gmail.com>
2023-05-02 21:28:59 -0500

    Amend router

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ const serve = app.listen(constants.HTTP_PORT, "localhost", function() {
     console.log("Server is running on port", serve.address().port);
 })
 
-app.get("/", function(req, res) {
-    res.status(parseInt(constants.HTTP_RESPONSE_OK)).sendFile(path.join(__dirname, "public/index.html"));
-});
+app.use(express.static(path.join(__dirname, "public")))
 
 app.post("/", function(req, res) {
     res.status(parseInt(constants.HTTP_RESPONSE_OK)).json({name: "John"});
 });
 
-app.use(express.static(path.join(__dirname, "public")))
+app.get("/", function(req, res) {
+    res.status(parseInt(constants.HTTP_RESPONSE_OK)).sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>404! Page not found</h1>');
+});
+
